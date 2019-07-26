31be7019f1832086955f426f7d4619a2b244e971
gegauno <gaaagggeee1121@gmail.com>
2019-07-26 14:34:39 +0800

    activity

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-document.getElementById("image").src = "off.gif";
+function light(sw) {
+    var pic;
+    if (sw == 0) {
+      pic = "img/off.gif"
+    } else {
+      pic = "img/on.gif"
+    }
+    document.getElementById('myImage').src = pic;
+  }
